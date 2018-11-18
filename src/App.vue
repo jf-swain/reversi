efedc41090a5481c9a76c23f4334079a4eb4821a
Jean-François Swain <swain.jean@gmail.com>
2018-11-18 23:10:22 +0100

    [MOD] (project) lancement project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,209 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="app">
+      <header class="header">
+        <div class="wrapper">
+          <div class="header__align">
+            <h1>Reversi</h1>
+            <h2 v-if="grid.length > 0">PLayer Turn : {{ player === 1 ? 'Noir' : 'Blanc'}}</h2>
+            <button @click="NewGame">New Game</button>
+          </div>
+        </div>
+      </header>
+
+      <div class="wrapper">
+        <article class="grid">
+          <div class="row" v-for="(item,i ) in grid" v-if="grid.length > 0" :key="i">
+            <Square v-for="(elmt, j) in item" :status="elmt" @clickSquare="checkElement($event)" v-if="elmt != null" :key="elmt.status.position"/>
+          </div>
+        </article>
+      </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Square from './components/Square.vue';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      grid: [],
+      row: 8,
+      col: 8,
+      player: 1,
+    };
+  },
   components: {
-    HelloWorld,
+    Square,
+  },
+  methods: {
+    initGrid() {
+      const { row, col } = this;
+      this.grid = [];
+
+      let grill = [];
+
+      for (let i = 1; i <= row; i++) {
+        grill[i] = [];
+
+        for (let j = 1; j <= col; j++) {
+          grill[i][j] = { status: 0, position: [i, j] };
+        }
+      }
+      return grill;
+    },
+    NewGame() {
+      const grill = this.initGrid();
+
+      grill[4][4].status = 2;
+      grill[4][5].status = 1;
+      grill[5][4].status = 1;
+      grill[5][5].status = 2;
+
+      this.grid = grill;
+    },
+    checkElement(data) {
+      const { position, status } = data;
+
+      const positionSquare = position;
+      const statusSquare = status;
+      const player = this.player;
+      if (!this.isEmpty(status)) {
+        return;
+      }
+
+      this.isValidPosition(position, player);
+    },
+    isEmpty(data) {
+      return data === 0;
+    },
+    isValidPosition(data, player) {
+      console.log(data, player);
+      const gridCheck = this.grid;
+      const row = data[0];
+      const col = data[1];
+      const status = player;
+      const statusReverse = player === 1 ? 2 : 1;
+      let rowCheck;
+      let colCheck;
+      let squarePosition = [];
+
+      for (let rowDir = -1; rowDir <= 1; rowDir++) {
+        for (let colDir = -1; colDir <= 1; colDir++) {
+          rowCheck = row + rowDir;
+          colCheck = col + colDir;
+
+          if (this.positionInGrid(rowCheck, colCheck)) {
+            if (this.checkStatus(rowCheck, colCheck) && this.checkPlayer(rowCheck, colCheck, statusReverse)) {
+              squarePosition.push([rowCheck, colCheck]);
+            }
+          }
+        }
+      }
+
+      console.log('spaq', squarePosition);
+
+      this.checkAround(squarePosition, [row, col], player);
+    },
+    checkAround(data, event, player) {
+      const playerInverse = player === 1 ? 2 : 1;
+      const dir = [];
+
+      for (let i = 0; i < data.length; i++) {
+        let x = data[i][0] - event[0];
+        let y = data[i][1] - event[1];
+
+        // check if next element in line is no empty
+        if (this.checkStatus(data[i][0] - -x, data[i][1] - -y)) {
+          // check if next element is ours
+          if (this.checkPlayer(data[i][0] - -x, data[i][1] - -y, player)) {
+            this.setColor(data[i][0], data[i][1], player);
+            this.setColor(event[0], event[1], player);
+            this.player = player === 1 ? 2 : 1;
+          }
+          const solution = [data[i][0], data[i][1]];
+          const newItemX = data[i][0] - -x;
+          const newItemY = data[i][1] - -y;
+
+          if (this.checkPlayer(data[i][0] - -x, data[i][1] - -y, playerInverse)) {
+            let arrrar = [];
+            while (
+              this.checkPlayer(data[i][0] - -x, data[i][1] - -y, playerInverse) &&
+              this.checkStatus(data[i][0] - -x, data[i][1] - -y)
+            ) {
+              arrrar.push([data[i][0] - -x, data[i][1] - -y]);
+              x += x;
+              y += y;
+            }
+
+            /// TODO: need to corrige it cos somtimes pb
+            for (var i = 0; i < arrrar.length; i++) {
+              this.setColor(arrrar[i][0], arrrar[i][1], player);
+              this.setColor(solution[0], solution[1], player);
+              this.setColor(newItemX, newItemY, player);
+              this.setColor(event[0], event[1], player);
+            }
+
+            this.player = player === 1 ? 2 : 1;
+          }
+        }
+      }
+    },
+    checkStatus(row, col) {
+      const gridCheck = this.grid;
+      return gridCheck[row][col].status !== 0;
+    },
+    checkPlayer(row, col, player) {
+      const gridCheck = this.grid;
+      return gridCheck[row][col].status === player;
+    },
+    setColor(row, col, player) {
+      this.grid[row][col].status = player;
+    },
+    positionInGrid(row, col) {
+      return row >= 1 && row <= this.row && (col >= 1 && col <= this.col);
+    },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.header {
+  border-bottom: 1px solid black;
+  color: white;
+  height: 6rem;
+
+  .wrapper {
+    height: 100%;
+  }
+
+  &__align {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+  }
+}
+
+button {
+  background-color: #ff2462;
+  border: 0;
+  color: #fff;
+  margin-bottom: 3rem;
+  padding: 10px 26px;
+}
+
+.grid {
+  display: flex;
+  max-width: 60rem;
+  margin-left: auto;
+  margin-right: auto;
+  flex-direction: column;
+
+  .row {
+    display: flex;
+    width: 100%;
+  }
 }
 </style>
---
+++ src/components/Square.vue
@@ -0,0 +1,78 @@
+<template>
+  <div
+    class="element"
+    :class="{'black': status.status === 1, 'white': status.status === 2}"
+    @click="clickSquare"
+    ></div>
+</template>
+
+<script>
+export default {
+  name: 'Square',
+  props: {
+    position: {
+      type: Array,
+    },
+    status: {
+      type: Object,
+      default() {
+        return {
+          status: 0,
+          position: [],
+        };
+      },
+    },
+  },
+  methods: {
+    clickSquare() {
+      this.$emit('clickSquare', this.status);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.element {
+  $width: 100%/8;
+
+  background: #2aa066;
+  border: 1px solid black;
+  box-sizing: border-box;
+  width: $width;
+
+  &:after {
+    content: '';
+    display: block;
+    margin-bottom: 100%;
+  }
+
+  &.black,
+  &.white {
+    &:before {
+      border-radius: 50%;
+      content: '';
+      display: block;
+      height: 75%;
+      margin-left: auto;
+      margin-right: auto;
+      position: relative;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 75%;
+    }
+  }
+  &.black {
+    &::before {
+      background: black;
+      border: 2px solid white;
+    }
+  }
+  &.white {
+    &::before {
+      background: white;
+      border: 2px solid black;
+    }
+  }
+}
+</style>
